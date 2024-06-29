84d1b0a5cd6462a5ef22c36489b054d954f41f48
Okimani <58261142+Okimani@users.noreply.github.com>
2024-06-29 03:53:29 +0300

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -1,9 +1,9 @@
-for name in  ['Oscar', 'Brenda', 'Linda', 'Sheila', 'Dorothy', 'Margaret', 'Gianna', 'Joyce']:
+for name in  ['Oscar', 'Jessica', 'Michael', 'Sheila', 'Liz', 'Violet', 'Gia', 'Malta']:
     print( 'Your name is ' + (name))
 
 #looping with a condition. This is not how while loop is typicall used. 
  #This is just for learning purposes.
-names =  ['Oscar', 'Brenda', 'Linda', 'Sheila', 'Dorothy', 'Margaret', 'Gianna', 'Joyce']
+names =  ['Mike', 'Jessica', 'Michael', 'Sheila', 'Liz', 'Violet', 'Gia', 'Malta']
 index = 0
 while index <len(names):
     print('Your name is ' + (names[index]))
